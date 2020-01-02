c7d64406b13a53016fc3777457b478d0ac31c328
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2020-01-02 17:58:26 -0500

    dice dynamically updating via state

--- src/RollDice.js
+++ src/RollDice.js
@@ -3,17 +3,41 @@ import Dice from './Dice';
 
 
 class RollDice extends Component {
+
+    static defaultProps = {
+        sides: [ "one", "two", "three", "four", "five", "six" ]
+    };
+
     constructor(props){
         super(props)
-        this.state = {dice1: "one", dice2: "two", dice3: "five" }
+        this.state = { dice1: "one", dice2: "two", dice3: "five" }
+        this.roll = this.roll.bind(this);
+    }
+
+    roll() {
+        // dynamically select numbers for each dice. 
+        const newDice1 = this.props.sides[
+            Math.floor(Math.random() * this.props.sides.length)
+        ];
+
+        const newDice2 = this.props.sides[
+            Math.floor(Math.random() * this.props.sides.length)
+        ];
+
+        const newDice3 = this.props.sides[
+            Math.floor(Math.random() * this.props.sides.length)
+        ];
+
+        // set state with new rolls
+        this.setState({dice1:newDice1, dice1:newDice2, dice3:newDice3});
     }
     render() {
         return (
             <div>
                 <Dice face={this.state.dice1}/>
                 <Dice face={this.state.dice2}/>
-                <Dice face={this.state.dice3}/>
-                
+                <Dice face={this.state.dice3}/> 
+                <button onClick={this.roll}>Roll Dice!</button>
             </div>
         )
     }
