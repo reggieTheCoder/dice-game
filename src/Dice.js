b19e1172e2cc16688bb332dc73d8e25424c39110
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2020-01-02 14:51:45 -0500

    font awesome added to project

--- src/Dice.js
+++ src/Dice.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 class Dice extends React.Component {
     render() {
         return <h1>Dice Game</h1>
+        <i className='fas fa-dice-one'/>
     }
 
 }
