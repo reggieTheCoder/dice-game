d9a3a6fda78dd6d8a5ca2045abcf55ca1873ed35
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2020-01-02 15:12:43 -0500

    font-awesome icon added to display

--- src/Dice.js
+++ src/Dice.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './Dice.css'
 class Dice extends React.Component {
     render() {
-        return <h1>Dice Game</h1>
-        <i className='fas fa-dice-one'/>
+        return (
+            <div>
+                <h1>Dice Game</h1>
+                <i className="fas fa-dice-one face"/>
+            </div>
+        
+        )
     }
 
 }
